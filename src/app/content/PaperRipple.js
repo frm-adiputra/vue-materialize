44cba9895760960fb00f273ca923799e26d8d8c6
Firmansyah Adiputra <frm.adiputra@gmail.com>
2016-02-14 23:16:46 +0700

    Highlight code examples

--- src/app/content/PaperRipple.js
+++ src/app/content/PaperRipple.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import PaperRipple from '../../md/PaperRipple'
 import marked from 'marked'
+import hljs from 'hljs'
 import url from 'file!./PaperRipplePage.md'
+
+marked.setOptions({
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value
+  }
+})
 
 Vue.component('paper-ripple-page', function (resolve, reject) {
   Vue.http({url: url, method: 'GET'}).then(function (response) {
